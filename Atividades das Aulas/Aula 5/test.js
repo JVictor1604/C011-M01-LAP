cb250f0f2bc2741b0b2c7d3151b482b155ff1559
JVictor1604 <josevictor8569@gmail.com>
2022-01-22 21:55:37 -0300

    commit 2

--- Atividades das Aulas/Aula 5/test.js
+++ Atividades das Aulas/Aula 5/test.js
@@ -2,13 +2,13 @@ const prompt = require('prompt-sync')();
 
 console.clear();
 
-let senha = 'Victor 2004'
+let senha = 2004
 
 let senha2 = 'oi'
 
-while (senha2 != 'Victor 2004') {senha2 = prompt('Diga a senha :')
+while (senha2 != senha ) {senha2 = +prompt('Diga a senha :')
 
-senha = senha2
+
 
 };
 
